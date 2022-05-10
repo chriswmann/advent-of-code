98beea6dd73a8aae9d770a6d70945f1dd8658262
Chris Mann <chrismwann@gmail.com>
2022-05-10 19:15:26 +0100

    Add day 2 and first part of day 3

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import os
 import sys
 from pathlib import Path
 
+from aoc_2021 import DayOne2021, DayTwo2021
+
 # directory reach
 current_file = Path(__file__)
 
@@ -10,9 +12,15 @@ sys.path.append(current_file.parent.absolute())
 
 from aoc_2021 import (
     DayOne2021,
+    DayTwo2021,
+    DayThree2021,
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-
+    print('Day 1, 2021')
     DayOne2021(year=2021, day=1)
+    print('Day 2, 2021')
+    DayTwo2021(year=2021, day=2)
+    print('Day 3, 2021')
+    DayThree2021(year=2021, day=3)
