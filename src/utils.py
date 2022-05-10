fafdf6862c79da12ebb04dfc8a7fb0d9559b228d
Chris Mann <chrismwann@gmail.com>
2022-05-10 19:12:09 +0100

    Add function to cast a list of bits to decimal.

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ from typing import Callable, List
 
 from aocd import get_data
 
+
 class SolutionCollection:
     """Utility class to store and print solutions.
 
@@ -11,12 +12,11 @@ class SolutionCollection:
 
     def __repr__(self):
 
-        solutions: List[str] = [f'{k}: {v}' for k, v in self.__dict__.items()]
-        return '\n'.join(solutions)
+        solutions: List[str] = [f"{k}: {v}" for k, v in self.__dict__.items()]
+        return "\n".join(solutions)
 
 
 class AbstractSolutionClass(ABC):
-
     def __init__(self, year, day) -> None:
 
         self.data: str = get_data(year=year, day=day)
@@ -27,10 +27,9 @@ class AbstractSolutionClass(ABC):
 
     @abstractmethod
     def prep_data(self) -> None:
-        """Prepare the raw data for processing.
-        """
+        """Prepare the raw data for processing."""
 
-        raise NotImplementedError('This is the abstract method.')
+        raise NotImplementedError("This is the abstract method.")
 
     def get_solutions(self) -> None:
         """Call the methods for each part.
@@ -42,8 +41,21 @@ class AbstractSolutionClass(ABC):
         """
 
         for method_name in dir(self):
-            if method_name.startswith('get_part_'):
+            if method_name.startswith("get_part_"):
                 method: Callable = getattr(self, method_name)
                 method()
 
         print(self.solutions)
+
+
+def decimal_from_list(binary):
+    """Convert a list of 0s and 1s to a decimal number.
+
+    Args:
+        binary (list[int]): List of 0s and 1s.
+
+    Returns:
+        int: Input as a decimal number.
+    """
+
+    return sum(val * (2**idx) for idx, val in enumerate(reversed(binary)))
